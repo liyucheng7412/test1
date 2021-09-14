9f3974b1ca5c4082561b770c91a4a3060baea815
lyc <liyucheng7412@qq.com>
2021-09-14 16:06:21 +0800

    tester02分支第一次提交

--- test/src/com/lyc/test2/c1.java
+++ test/src/com/lyc/test2/c1.java
@@ -4,6 +4,6 @@ public class c1 {
     public static void main(String[] args) {
         System.out.println("第二次提交");
 
-        System.out.println("tester01分支第二次提交");
+        System.out.println("tester02分支第一次提交");
     }
 }
